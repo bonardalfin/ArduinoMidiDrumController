d531f3540ae5db0450cc99eb72ea782d786f9eb1
Bonard Alfin <156160403+bonardalfin@users.noreply.github.com>
2025-09-26 01:50:12 +0700

    Update LEONARDO_1.0.0.ino

--- LEONARDO_1.0.0/LEONARDO_1.0.0.ino
+++ LEONARDO_1.0.0/LEONARDO_1.0.0.ino
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Dibuat oleh Bonard Alfin
-// perfectsilentproject.blogspot.com
+// bonardalfinproject.blogspot.com
 
 ////////////////////////////////////////////////////////////////////////////////
 // Untuk Arduino Leonardo
@@ -242,3 +242,4 @@ void printDebug(byte padIndex, int value, const char* state) {
   Serial.print("] Value: ");
   Serial.println(value);
 }
+
